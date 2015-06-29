3bb90018a9360bc13aca554974360b10f940d4fc
timhyd <hydanustd@mail.masters.edu>
2015-06-28 21:10:32 -0700

    Chapter 4 assignment started.

---
+++ Assignments/ch4h23.java
@@ -0,0 +1,11 @@
+//Original Programming - Timothy Hydanus, Head Programmer GoalPost Programming
+//Modified assignment: Desplay employee name in UPPER Case. And display curence amounts to two decimal places.
+import java.util.*;
+public class ch3h29{
+   public static void main(String[] args){
+      Scanner input = new Scanner(System.in);
+   
+
+   //End code
+   }
+}
