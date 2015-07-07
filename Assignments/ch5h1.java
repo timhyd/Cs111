eb1fb146889023cd579954c93502fa7a68c6de4d
timhyd <hydanustd@mail.masters.edu>
2015-07-07 11:44:19 -0700

    Ch5 assignmnet almost complete

--- Assignments/ch5h1.java
+++ Assignments/ch5h1.java
@@ -1,6 +1,6 @@
 //Original Code by Timothy Hydanus, Lead Programmmer - Goalpost Programming
 import java.util.*;
-public class ch1h3{
+public class ch5h1{
    public static void main(String[] args){
       int posNums = 0;
       int negNums = 0;
@@ -11,9 +11,9 @@ public class ch1h3{
       //Prompt for string
       System.out.print("Enter all terms separated by spaces and ending with a zero");
       String startString = input.nextLine();
-      if startString(startString.length-1) != "0"{
+      if (startString(startString.length-1) != "0"){
          while(startString(startString.length-1) != "0"){
-            System.out.prinln("");
+            System.out.println("");
             System.out.print("Error: Incorrect Input.");
             System.out.print("Enter all terms separated by spaces and ending with a zero");
             String startString = input.nextLine();
@@ -21,18 +21,27 @@ public class ch1h3{
          }
       //End if
       }
+
+      for(int j=0; j<(splitResult.length); j++){
+         splitResult[j] = Integer.parseInt(splitResult[j]);
+      //End for loop
+      }
+
       //Split the Result up into it's parts
       splitResult = startString.split(delimiter);
       for(int i=0; i<splitResult.length; i++){
-         if (splitResult(i)>0){
+         if (splitResult[i]>0){
             posNums++;
          //End if
          }
-         if(splitResult(i)<0){
+         if(splitResult[i]<0){
             negNums++;
          }
       //End for loop
       }
+
+      System.out.println("There are "+posNums+" Positive Numbers.");
+      System.out.println("There are "+negNums+" Negative Numbers.");
 
       //End Code
       }
