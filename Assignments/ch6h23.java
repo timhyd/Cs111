ec37abafca94082a58e26e65896755dce8433b43
timhyd <hydanustd@mail.masters.edu>
2015-07-20 22:10:30 -0700

    Start chapter 6

---
+++ Assignments/ch6h23.java
@@ -0,0 +1,24 @@
+//Original Code by Timothy Hydanus, Lead Programmmer - Goalpost Programming
+import java.util.*;
+public class ch5h1{
+   public static void main(String[] args){
+
+   public static int count(String str, char a){
+   //Start Count method
+      int result = 0;
+      for (int i = 0; i = str.length()){
+         if (str.charAt(i) == a){
+            result++;
+            //end if
+         }
+      //end for
+      }
+      return result;
+   //End count method
+   }
+
+
+   System.out.println("The Character " + a + " occurs " + result)
+   //End Code
+   }
+}
