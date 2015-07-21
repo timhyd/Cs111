41bc804e371baafeb512b4f109550b633dea1cfe
timhyd <hydanustd@mail.masters.edu>
2015-07-21 00:06:17 -0700

    update

--- Assignments/ch6h23.java
+++ Assignments/ch6h23.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class ch6h23{
    public static void main(String[] args){
    Scanner input = new Scanner(System.in);
-   int continue = 1;
    String foo = " ";
    Char bar = "a";
 
@@ -12,6 +11,12 @@ public class ch6h23{
    bar = input.next().charAt(0);
    int result = count(foo, bar);
 
+
+
+
+   System.out.println("The Character " + a + " occurs " + result + " times.");
+   //End main
+   }
    public static int count(String str, char a){
    //Start Count method
       int result = 0;
@@ -24,10 +29,5 @@ public class ch6h23{
       }
       return result;
    //End count method
-   }
-
-
-   System.out.println("The Character " + a + " occurs " + result + " times.");
-   //End Code
    }
 }
