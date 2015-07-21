ad5f11067d10e233bf10b6902efce244a3603772
timhyd <hydanustd@mail.masters.edu>
2015-07-20 23:40:55 -0700

    Newest ch6 work

--- Assignments/ch6h23.java
+++ Assignments/ch6h23.java
@@ -1,7 +1,16 @@
 //Original Code by Timothy Hydanus, Lead Programmmer - Goalpost Programming
 import java.util.*;
-public class ch5h1{
+public class ch6h23{
    public static void main(String[] args){
+   Scanner input = new Scanner(System.in);
+   int continue = 1;
+   String foo = " ";
+   Char bar = "a";
+
+   Sysem.out.println("Enter the String you would like to use below:");
+   foo = input.nextLine;
+   bar = input.next().charAt(0);
+   int result = count(foo, bar);
 
    public static int count(String str, char a){
    //Start Count method
@@ -18,7 +27,7 @@ public class ch5h1{
    }
 
 
-   System.out.println("The Character " + a + " occurs " + result)
+   System.out.println("The Character " + a + " occurs " + result + " times.");
    //End Code
    }
 }
