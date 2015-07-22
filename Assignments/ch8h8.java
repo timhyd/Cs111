7ecdb0a0848c8a201491e3a6936f1fd465ebe222
timhyd <hydanustd@mail.masters.edu>
2015-07-22 00:47:31 -0700

    start chapter 8

---
+++ Assignments/ch8h8.java
@@ -0,0 +1,12 @@
+//Original Code by Timothy Hydanus, Lead Programmmer - Goalpost Programming
+import java.util.*;
+public class ch7h31{
+   public static void main(String[] args){
+     Scanner input = new Scanner(System.in);
+     
+
+  //end main
+  }
+
+//End code
+}
