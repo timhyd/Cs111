47e9cab91802bdcd930562331fab1b7958580c94
timhyd <hydanustd@mail.masters.edu>
2015-07-26 13:57:16 -0700

    chapter 10
    Stringbuilder1 started

--- Assignments/ch10h27/stringBuilder.java
+++ Assignments/ch10h27/stringBuilder.java
@@ -1,10 +1,42 @@
 //Original Code by Timothy Hydanus, Lead Programmmer - Goalpost Programming
 import java.util.*;
-public class stringBuilder{
-   public static void main(String[] args){
-     Scanner input = new Scanner(System.in);
+public class stringBuilder1{
+   public stringBuilder{
+
+      public String MyStringBuilder1(String S){
+         return StringBuilder(s);
+      }//End MyStringBuilder1
+
+      public append(int i){
+         this.append(i);
+      }// End append
+
+      public int length(){
+         return this.length();
+      }// end lenght
+
+      public char charAt(int index){
+         return this.charAt(index);
+      }// End charAt
+
+      public toLowerCase(){
+         return this.toLowerCase();
+
+      }// End toLowerCase
+
+      public String substring(int begin, int end){
+         return this.substring(begin, end);+
+
+      }
+
+      public String toString(){
+         return this.toString();
+      }
 
 
 
-  }// end main
+   }//End stringBuilder1
+
+
+
 }// end Code
--- Assignments/ch11h1.java
+++ Assignments/ch11h1.java
@@ -1,10 +1,9 @@
-//Original Code by Timothy Hydanus, Lead Programmmer - Goalpost Programming
+/Original Code by Timothy Hydanus, Lead Programmmer - Goalpost Programming
 import java.util.*;
 public class ch9h1{
    public static void main(String[] args){
      Scanner input = new Scanner(System.in);
-
      
 
-  }// end main
-} // end Code
+  }//End main
+}//end Code
