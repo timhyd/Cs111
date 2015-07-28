10a9b5845eeba1a2f2761c92d424d1e78a6eb2e7
timhyd <hydanustd@mail.masters.edu>
2015-07-27 18:05:39 -0700

    STILL NOT WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Assignments/ch10h27/MystringBuilder1.java
+++ Assignments/ch10h27/MystringBuilder1.java
@@ -2,10 +2,12 @@
 import java.util.*;
 public class MystringBuilder1{
 
-      public String MyStringBuilder1(String S){
-         return StringBuilder(s);
+      public String StringBuilder(String s){
+         return this.StringBuilder(s);
       }//End MyStringBuilder1
-
+      public String append(String str){
+         return this.append(str);
+      }// End append
       public String append(int i){
          return this.append(i);
       }// End append
--- Assignments/ch10h27/ch10h27.java
+++ Assignments/ch10h27/ch10h27.java
@@ -1,10 +1,31 @@
 //Original Code by Timothy Hydanus, Lead Programmmer - Goalpost Programming
 import java.util.*;
-public class ch9h1{
+public class ch10h27{
    public static void main(String[] args){
      Scanner input = new Scanner(System.in);
+     System.out.println("This Programm shows that my class MystringBuilder1 does function properly");
+     System.out.println("Please Enter a string of less than 16 characters.");
 
-     
+     // Input the inputed string
+     String theString = input.nextLine();
+     System.out.println("Thanks! Now I'm gonna do a ton of stuff to it!");
+     MystringBuilder1 theStringObject = new MystringBuilder1();
+     theStringObject.
+     StringBuilder(theString);
+
+     System.out.println("Now I am going to append to your string");
+     System.out.println(theStringObject.append(theString));
+
+     System.out.println("Now I am going to append to your string again" + "This time with the length of your inputed string");
+     System.out.println( theStringObject.append( theStringObject.length() ) );
+
+     System.out.println("Now I am going to show you which character is at a given point in your string");
+     System.out.println("Please give a number between 0 and " + (theStringObject.length()-1) );
+     int index = input.nextInt();
+     System.out.println(theStringObject.charAt(index));
+
+     System.out.println("Now I will show you a substring of your input using your previously given number");
+     System.out.println(theStringObject.substring(0,index));
 
   }// end main
 } // end Code
