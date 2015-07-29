fc9d6a7ee28b4b7be4641b7721ef1fbabd423af8
timhyd <hydanustd@mail.masters.edu>
2015-07-28 22:02:28 -0700

    Update

--- Assignments/ch10h27/ch10h27.java
+++ Assignments/ch10h27/ch10h27.java
@@ -13,11 +13,11 @@ public class ch10h27{
      theStringObject.MystringBuilder1(theString);
 
      System.out.println("Now I am going to append to your string");
-     System.out.println(theStringObjectAppend1);
+     System.out.println(theStringObjectAppend1.theString);
 
      System.out.println("Now I am going to append to your string again" + "This time with the length of your inputed string");
      theStringObject.append( theStringObject.length());
-     System.out.println(theStringObjectAppend2);
+     System.out.println(theStringObjectAppend2.theString);
 
      System.out.println("Now I am going to show you which character is at a given point in your string");
      System.out.println("Please give a number between 0 and " + (theStringObject.length()-1) );
@@ -25,7 +25,7 @@ public class ch10h27{
      System.out.println(theStringObject.charAt(index));
 
      System.out.println("Now I will show you a substring of your input using your previously given number");
-     System.out.println(theStringSub);
+     System.out.println(theStringSub.theString);
 
   }// end main
 } // end Code
