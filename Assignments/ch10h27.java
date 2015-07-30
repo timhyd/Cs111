4b8d0e430f01b6e30eb9345011dbdf029601ca51
timhyd <hydanustd@mail.masters.edu>
2015-07-30 00:14:31 -0700

    CHapter 10 Complete!!!!!!!!!!!!!

--- Assignments/ch10h27.java
+++ Assignments/ch10h27.java
@@ -26,19 +26,20 @@ public class ch10h27{
      theAppendedObject = theStringObject.append(theStringObject);
      System.out.println(theAppendedObject.theString);
 
-     System.out.println("Now I am going to append to your string again" + "This time with the length of your inputed string");
+     System.out.println("Now I am going to append to your string again. " + "This time with the length of your inputed string");
      theStringObject = theStringObject.append( theStringObject.length());
      System.out.println(theStringObject.theString);
 
      System.out.println("Now I am going to show you which character is at a given point in your string");
      System.out.println("Please give a number between 0 and " + (theStringObject.length()-1) );
      int index = input.nextInt();
+     System.out.print("The Character at: " + index +" is: ");
      System.out.println(theStringObject.charAt(index));
 
      System.out.println("Now I will show you a substring of your input using your previously given number");
      MystringBuilder1 theSubString = new MystringBuilder1();
      theSubString = theStringObject.substring(0,index);
-     System.out.println(theStringObject.theString);
+     System.out.println(theSubString.theString);
 
      System.out.println("Now I will Pull the String out of the object.");
      String theModdedString = theStringObject.theString;
